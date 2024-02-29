77dea41d537a551436310572a972ff77df129838
kijai <40791699+kijai@users.noreply.github.com>
2024-02-29 14:08:13 +0200

    Update nodes.py

--- nodes.py
+++ nodes.py
@@ -38,7 +38,7 @@ class SUPIR_Upscale:
                 'AdaIn',
                 'Wavelet',
             ], {
-               "default": 'Wavelet'
+               "default": 'AdaIn'
             }),
             "keep_model_loaded": ("BOOLEAN", {"default": True}),
             "use_tiled_vae": ("BOOLEAN", {"default": True}),
@@ -65,6 +65,7 @@ class SUPIR_Upscale:
         
         device = comfy.model_management.get_torch_device()
         image = image.to(device)
+        batch_size = image.shape[0]
         SUPIR_MODEL_PATH = folder_paths.get_full_path("checkpoints", supir_model)
         SDXL_MODEL_PATH = folder_paths.get_full_path("checkpoints", sdxl_model)
         
@@ -92,6 +93,7 @@ class SUPIR_Upscale:
             config.model.params.ae_dtype = vae_dtype
             config.model.params.diffusion_dtype = model_dtype
             self.model = instantiate_from_config(config.model).cpu()
+            print(type(self.model))
             from .SUPIR.util import load_state_dict
             supir_state_dict = load_state_dict(SUPIR_MODEL_PATH)
             sdxl_state_dict = load_state_dict(SDXL_MODEL_PATH)
@@ -103,31 +105,36 @@ class SUPIR_Upscale:
    
         autocast_condition = dtype == torch.float16 or torch.bfloat16 and not comfy.model_management.is_device_mps(device)
         with torch.autocast(comfy.model_management.get_autocast_device(device), dtype=dtype) if autocast_condition else nullcontext():
+            
             image, = ImageScaleBy.upscale(self, image, resize_method, scale_by)
             B, H, W, C = image.shape
             new_height = H // 64 * 64
             new_width = W // 64 * 64
             image = image.permute(0, 3, 1, 2).contiguous()
             resized_image = F.interpolate(image, size=(new_height, new_width), mode='bicubic', align_corners=False)
-            
+                
             captions_list = []
             captions_list.append(captions)
             print(captions_list)
-            
+                
             use_linear_CFG = cfg_scale_start > 0
             use_linear_control_scale = control_scale_start > 0
-            # # step 3: Diffusion Process
-            samples = self.model.batchify_sample(resized_image, captions_list, num_steps=steps, restoration_scale= restoration_scale, s_churn=s_churn,
-                                            s_noise=s_noise, cfg_scale=cfg_scale, control_scale=control_scale, seed=seed,
-                                            num_samples=1, p_p=a_prompt, n_p=n_prompt, color_fix_type=color_fix_type,
-                                            use_linear_CFG=use_linear_CFG, use_linear_control_scale=use_linear_control_scale,
-                                            cfg_scale_start=cfg_scale_start, control_scale_start=control_scale_start)
-            # save
+            out = []
+            pbar = comfy.utils.ProgressBar(B)
+            for i in range(B):
+                # # step 3: Diffusion Process
+                samples = self.model.batchify_sample(resized_image[i].unsqueeze(0), captions_list, num_steps=steps, restoration_scale= restoration_scale, s_churn=s_churn,
+                                                s_noise=s_noise, cfg_scale=cfg_scale, control_scale=control_scale, seed=seed,
+                                                num_samples=1, p_p=a_prompt, n_p=n_prompt, color_fix_type=color_fix_type,
+                                                use_linear_CFG=use_linear_CFG, use_linear_control_scale=use_linear_control_scale,
+                                                cfg_scale_start=cfg_scale_start, control_scale_start=control_scale_start)
+                
+                out.append(samples.squeeze(0).cpu())
+                pbar.update(1)
             if not keep_model_loaded:
-                self.model = None
-            samples = samples.permute(0, 2, 3, 1).cpu()
-            
-            return(samples,)
+                    self.model = None
+            out_stacked = torch.stack(out, dim=0).cpu().to(torch.float32).permute(0, 2, 3, 1)
+            return(out_stacked,)
     
 NODE_CLASS_MAPPINGS = {
     "SUPIR_Upscale": SUPIR_Upscale,
