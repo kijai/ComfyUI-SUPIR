29f2e8be0e9c32197ab6cf3a79a8c5776867d8f0
kijai <40791699+kijai@users.noreply.github.com>
2025-04-14 01:52:00 +0300

    Possible workaround for import issue in some configurations

--- sgm/util.py
+++ sgm/util.py
@@ -177,14 +177,17 @@ def instantiate_from_config(config):
 
 
 def get_obj_from_str(string, reload=False, invalidate_cache=True):
-    package_directory_name = os.path.basename(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
     module, cls = string.rsplit(".", 1)
     if invalidate_cache:
         importlib.invalidate_caches()
     if reload:
         module_imp = importlib.import_module(module)
         importlib.reload(module_imp)
-    return getattr(importlib.import_module(module, package=package_directory_name), cls)
+    try:
+        obj = getattr(importlib.import_module(module, package=os.path.basename(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))), cls)
+    except:
+        obj = getattr(importlib.import_module(module, package=os.path.dirname(os.path.dirname(os.path.abspath( __file__ )))), cls)
+    return obj
 
 
 def append_zero(x):
